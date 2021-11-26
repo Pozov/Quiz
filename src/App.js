6fe3d93eccbf7177f1161bbd8d798885313f0a99
ruslanpozov <rusik10032000@mail.ru>
2021-11-26 17:37:40 +0300

    Router Практика. 71 урок

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import Layout from "./hoc/Layout/Layout";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {Route, Switch} from "react-router-dom"
 import Quiz from "./containers/Quiz/Quiz";
 import Auth from "./containers/Auth/Auth";
 import QuizList from "./containers/QuizList/QuizList";
@@ -8,14 +8,12 @@ import QuizCreator from "./containers/QuizCreator/QuizCreator";
 
 const App = () => (
             <Layout>
-                <Router>
                     <Switch>
-                        <Route path="/auth" > <Auth/> </Route>
-                        <Route path="/quiz-creator" > <QuizCreator/> </Route>
-                        <Route path="/quiz/:id" > <Quiz/> </Route>
-                        <Route path="/" > <QuizList/> </Route>
+                        <Route path="/auth" component={Auth} />
+                        <Route path="/quiz-creator" component={QuizCreator} />
+                        <Route path="/quiz/:id" component={Quiz} />
+                        <Route path="/" component={QuizList} />
                     </Switch>
-                </Router>
              </Layout>
   )
 
