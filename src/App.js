9eb75b47056a4d479167eda4ae3b1db55fd11fb9
ruslanpozov <rusik10032000@mail.ru>
2021-11-26 17:12:24 +0300

    Router Практика. 70 урок

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, {Component} from "react";
 import Layout from "./hoc/Layout/Layout";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Quiz from "./containers/Quiz/Quiz";
+import Auth from "./containers/Auth/Auth";
+import QuizList from "./containers/QuizList/QuizList";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
 
-class App extends Component {
-  render () {
-      return (
-        <Layout>
-            <Quiz/>
-        </Layout>
-  );
-}
-}
+const App = () => (
+            <Layout>
+                <Router>
+                    <Switch>
+                        <Route path="/auth" > <Auth/> </Route>
+                        <Route path="/quiz-creator" > <QuizCreator/> </Route>
+                        <Route path="/quiz/:id" > <Quiz/> </Route>
+                        <Route path="/" > <QuizList/> </Route>
+                    </Switch>
+                </Router>
+             </Layout>
+  )
+
+
 
 export default App;
